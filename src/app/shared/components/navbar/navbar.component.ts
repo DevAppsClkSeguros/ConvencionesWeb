789d620c45238f3afac7cb288fa1a415d31aa76d
Oscar Mireles <oscar.mireles@grupobituaj.com.mx>
2025-07-07 14:29:51 -0600

    Agregando nombre de versión

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { AppConfig } from '@shared/app-config';
 import { AuthService } from '@core/interceptor/auth.service';
+import { environment } from 'src/environments/environment'
 
 @Component({
   selector: 'shared-navbar',
@@ -12,6 +13,7 @@ export class NavbarComponent implements OnInit {
   authService = inject(AuthService);
   router = inject(Router);
   nombreUsuario: string = '';
+  environment = environment;
 
   ngOnInit(): void {
     const datosUsuario = this.authService.getUserData();
