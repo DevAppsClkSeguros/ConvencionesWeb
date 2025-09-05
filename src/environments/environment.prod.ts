7e8ccb25856bdd0f58e2b2af7f1882def80cc327
Oscar Mireles <oscar.mireles@grupobituaj.com.mx>
2025-09-05 12:13:08 -0600

    cambio de versiones

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  version: '1.3.5',
+  version: '1.3.6',
   apiUrl: 'https://api.clks.lat/Convenciones',
   // apiUrl: 'https://dev.developmentservices.com.mx/ApisConvenciones',
   microsoftGraphUrl: 'https://graph.microsoft.com/v1.0/users/',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  version: '1.0.2',
+  version: '1.0.3',
   // apiUrl: 'https://api.clks.lat/Convenciones/',
   apiUrl: 'https://dev.developmentservices.com.mx/ApisConvenciones',
   microsoftGraphUrl: 'https://graph.microsoft.com/v1.0/users/',
